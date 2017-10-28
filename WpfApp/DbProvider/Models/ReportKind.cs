102d8b4712c82c972b0e9eae993e5439d35b3523
alevya <alexv47@mail.ru>
2017-10-28 20:17:10 +0300

    commit

--- WpfApp/DbProvider/Models/ReportKind.cs
+++ WpfApp/DbProvider/Models/ReportKind.cs
@@ -1,14 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace WpfApp.DbProvider.Models
+﻿namespace WpfApp.DbProvider.Models
 {
     public class ReportKind
     {
         public string Name { get; set; }
         public string Description { get; set; } 
+        public bool IsSelected { get; set; }
     }
 }
--- WpfApp/WpfApp/ViewModel/ReportWorkspaceViewModel.cs
+++ WpfApp/WpfApp/ViewModel/ReportWorkspaceViewModel.cs
@@ -9,7 +9,7 @@ namespace WpfApp.ViewModel
         {
             ReportKinds = new List<ReportKind>
                           {
-                              new ReportKind{Name = "Отчет по источникам", Description = "Отчет по общему количеству событий от выбранных источников"},
+                              new ReportKind{Name = "Отчет по источникам", Description = "Отчет по общему количеству событий от выбранных источников", IsSelected = true},
                               new ReportKind{Name = "Отчет по уведомлениям", Description = "Отчет по количеству событий для каждой из групп уведомлений"},
                               new ReportKind{Name = "Отчет по IP-адресам", Description = "Отчет по общему количеству событий от каждого IP-адреса"},
                               new ReportKind{Name = "Графики <X,Y> событий", Description = @"Графики вида ""Время(Ось X)-Количество событий(Ось Y)"""},
