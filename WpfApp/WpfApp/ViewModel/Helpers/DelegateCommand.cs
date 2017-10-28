7697f10ff5391d1c8d51a6be7dfebb0d7499d21b
alevya <alexv47@mail.ru>
2017-10-28 23:04:16 +0300

    comm

---
+++ WpfApp/WpfApp/ViewModel/Helpers/DelegateCommand.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Windows.Input;
+
+namespace WpfApp.ViewModel.Helpers
+{
+    internal class DelegateCommand : ICommand
+    {
+        private readonly Predicate<object> _canExecPredicate;
+        private readonly Action<object> _execAction;
+
+        public DelegateCommand(Action<object> execute) : this(execute, null)
+        {
+        }
+
+        public DelegateCommand(Action<object> execute, Predicate<object> predicate)
+        {
+            _execAction = execute ?? throw new ArgumentNullException(nameof(execute));
+            _canExecPredicate = predicate;
+        }
+
+        #region ICommand Implements
+        public bool CanExecute(object parameter)
+        {
+            return _canExecPredicate?.Invoke(parameter) ?? false;
+        }
+
+        public void Execute(object parameter)
+        {
+            if (!CanExecute(parameter))
+            {
+                throw new InvalidOperationException("Команда не действительна для выполнения");
+            }
+
+            _execAction(parameter);
+        }
+        public event EventHandler CanExecuteChanged
+        {
+            add => CommandManager.RequerySuggested += value;
+            remove => CommandManager.RequerySuggested -= value;
+        }
+        //public event EventHandler CanExecuteChanged;
+        #endregion
+    }
+}
--- WpfApp/WpfApp/ViewModel/ReportWorkspaceViewModel.cs
+++ WpfApp/WpfApp/ViewModel/ReportWorkspaceViewModel.cs
@@ -1,10 +1,13 @@
 ﻿using System.Collections.Generic;
+using System.Windows.Input;
 using WpfApp.DbProvider.Models;
+using WpfApp.ViewModel.Helpers;
 
 namespace WpfApp.ViewModel
 {
     internal class ReportWorkspaceViewModel : ViewModelBase
     {
+        #region Init
         public ReportWorkspaceViewModel()
         {
             ReportKinds = new List<ReportKind>
@@ -14,12 +17,23 @@ namespace WpfApp.ViewModel
                               new ReportKind{Name = "Отчет по IP-адресам", Description = "Отчет по общему количеству событий от каждого IP-адреса"},
                               new ReportKind{Name = "Графики <X,Y> событий", Description = @"Графики вида ""Время(Ось X)-Количество событий(Ось Y)"""},
                           };
+            FilterReport = new FilterReportViewModel();
+            CreateReportCommand = new DelegateCommand(o => _createReport());
         }
+        #endregion
 
         #region Properties
-
         public IEnumerable<ReportKind> ReportKinds { get; }
+        public FilterReportViewModel FilterReport { get; }
+        #endregion
 
+        #region Command
+        public ICommand CreateReportCommand { get; }
+
+        private void _createReport()
+        {
+            
+        }
         #endregion
     }
 }
