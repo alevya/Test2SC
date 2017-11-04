54d798c7408e92d79ce168e41a3c263e9a6b4fd9
alevya <alexv47@mail.ru>
2017-11-04 20:16:42 +0300

    comm

---
+++ TestReport/TestReportApp/View/CheckDetailView.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System.Windows.Controls;
+
+namespace TestReportApp.View
+{
+    /// <summary>
+    /// Логика взаимодействия для SystemTableDetailsView.xaml
+    /// </summary>
+    public partial class CheckDetailView : UserControl
+    {
+        public CheckDetailView()
+        {
+            InitializeComponent();
+        }
+    }
+}
