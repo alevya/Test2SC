67f0a903c8455df69f9f2072291d4be39d2276c0
alevya <alexv47@mail.ru>
2017-11-05 21:05:25 +0300

    delete unused model

--- TestReport/DbProvider/ReportContext.cs
+++ TestReport/DbProvider/ReportContext.cs
@@ -22,12 +22,6 @@ namespace TestReportApp.DbProvider
             }
         }
 
-        //public DbSet<Correlation.CorrelationSuspicious> CorrelationsSuspicious { get; set; }
-        //public DbSet<Correlation.CorrelationSuspiciousHigh> CorrelationsSuspiciousHigh { get; set; }
-        //public DbSet<Correlation.CorrelationAwareness> CorrelationsAwareness { get; set; }
-        //public DbSet<Correlation.CorrelationAwarenessHigh> CorrelationsAwarenessHigh { get; set; }
-        //public DbSet<Correlation.CorrelationAnalyze> CorrelationsAnalyze { get; set; }
-
         public DbSet<SystemTables.SystemTable> SystemTables { get; set; }
         public DbSet<SystemTables.SystemNotificationGroup> SystemNotificationGroups { get; set; }
 
